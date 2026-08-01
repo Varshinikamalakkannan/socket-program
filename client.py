c474874e1cce1a02cf70f1e74d4644fe6204fa73
Varshini K <varshinikamalakkannan@gmail.com>
2026-08-01 17:52:20 +0530

    Implement basic TCP client in client.py

---
+++ client.py
@@ -0,0 +1,22 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+print("Connected to server.")
+
+while True:
+    message = input("Enter message: ")
+
+    client.send(message.encode())
+
+    response = client.recv(1024).decode()
+    print("Server:", response)
+
+    if message.lower() == "exit":
+        break
+
+client.close()
